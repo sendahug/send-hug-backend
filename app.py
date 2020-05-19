7bdbdf363dcaf7b0b4728f8991ee2b190e327c6c
Shir Bar Lev <shirblc@gmail.com>
2020-05-19 19:46:53 +0300

    Added home route.

--- app.py
+++ app.py
@@ -20,6 +20,33 @@ def create_app(test_config=None):
 
         return response
 
+    # Routes
+    # -----------------------------------------------------------------
+    # Endpoint: GET /
+    # Description: Gets recent and suggested posts.
+    # Parameters: None.
+    @app.route('/')
+    def index():
+        # Gets the ten most recent posts
+        recent_posts = Post.query.order_by(Post.date).limit(10).all()
+        formatted_recent_posts = []
+
+        for post in recent_posts:
+            formatted_recent_posts.append(post.format())
+
+        # Gets the ten posts with the least hugs
+        suggested_posts = Post.query.order_by(Post.given_hugs).limit(10).all()
+        formatted_suggested_posts = []
+
+        for post in suggestted_posts:
+            formatted_suggested_posts.append(post.format())
+
+        return jsonify({
+            'success': True,
+            'recent': formatted_recent_posts,
+            'suggested': formatted_suggested_posts
+        })
+
     return app
 
 APP = create_app()
