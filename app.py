875d30f1ec3db9e3a831faf5ed310d0cb6ef8813
Shir Bar Lev <shiruchii@gmail.com>
2020-05-19 18:36:01 +0300

    Minor fixes.
    -Styling fixes.
    -Moved Flask-SQLAlchemy import to the models file.

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import os
 from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app)
 
-  return app
+    return app
 
 APP = create_app()
 
