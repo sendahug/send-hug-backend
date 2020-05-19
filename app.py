b165d837f431b79233768b99a59491bf016af84a
Shir Bar Lev <shirblc@gmail.com>
2020-05-19 19:15:15 +0300

    Connected the models file to the main server file.

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 
+from models import create_db, Post, User, Message
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    create_db(app)
     CORS(app)
 
     # CORS Setup
