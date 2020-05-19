771ebdeee2f192d96c6d673cd9f1cb1fe10e34ba
Shir Bar Lev <shiruchii@gmail.com>
2020-05-19 18:41:51 +0300

    Setup Flask-CORS to enable frontend communication.

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ def create_app(test_config=None):
     app = Flask(__name__)
     CORS(app)
 
+    # CORS Setup
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', 'localhost:5000')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST,\
+                              PATCH, DELETE, OPTIONS')
+        response.headers.add('Access-Control-Allow-Headers', 'Authorization')
+
+        return response
+
     return app
 
 APP = create_app()
