499f432961434741e92c1ef78081591ad7c91144
Shir Bar Lev <shiruchii@gmail.com>
2020-05-19 18:26:11 +0300

    Added base models.

--- models.py
+++ models.py
@@ -1,1 +1,46 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
 
+# Database configuration
+databate_username = os.environ.get('DBUSERNAME')
+database_name = 'capstone'
+database_path = 'postgres://{}@localhost:5432/{}'.format(databate_username,
+                                                         database_name)
+
+db = SQLAlchemy()
+
+
+# Database setup
+def create_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
+
+# Models
+# -----------------------------------------------------------------
+# Post Model
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    text = db.Column(db.String(480), nullable=False)
+
+
+# User Model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    auth0_id = db.Column(db.String(), nullable=False)
+    received_hugs = db.Column(db.Integer, default=0)
+    given_hugs = db.Column(db.Integer, default=0)
+    posts = db.relationship('Post', backref='user')
+
+
+# Message Model
+class Message(db.Model):
+    __tablename__ = 'messages'
+    id = db.Column(db.Integer, primary_key=True)
+    from_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    for_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    text = db.Column(db.String(480), nullable=False)
