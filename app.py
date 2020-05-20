0d6dcb6d9f7330dc07a251a4a9255f35ff304497
Shir Bar Lev <shiruchii@gmail.com>
2020-05-20 15:59:28 +0300

    Added POST endpoints for adding new records to the database.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 
@@ -57,6 +57,32 @@ def create_app(test_config=None):
         })
 
 
+    # Endpoint: POST /posts
+    # Description: Add a new post to the database.
+    # Parameters: None.
+    @app.route('/posts', methods=['POST'])
+    def add_post():
+        # Get the post data and create a new post object
+        new_post_data = json.loads(request.data)
+        new_post = Post(user_id=new_post_data['userId'],
+                        text=new_post_data['text'],
+                        date=new_post_data['date'],
+                        given_hugs=new_post_data['hugs'])
+
+        # Try to add the post to the database
+        try:
+            db_add(new_post)
+            added_post = new_post.format()
+        # If there's an error, abort
+        except Exception as e:
+            abort(500)
+
+        return jsonify({
+            'success': True,
+            'posts': added_post
+        })
+
+
     # Endpoint: GET /users
     # Description: Gets the user's data.
     # Parameters: None.
@@ -79,6 +105,31 @@ def create_app(test_config=None):
         return jsonify({
             'success': True,
             'user': formatted_user_data
+        })
+
+
+    # Endpoint: POST /users
+    # Description: Adds a new user to the users table.
+    # Parameters: None.
+    @app.route('/users', methods=['POST'])
+    def add_user():
+        # Gets the user's data via the Auth0 post-registration hook
+        user_data = json.loads(request.data)
+        new_user = User(username=user_data['username'],
+                        auth0_id=user_data['id'], received_hugs=0,
+                        given_hugs=0, posts=0)
+
+        # Try to add the post to the database
+        try:
+            db_add(new_user)
+            added_user = new_user.format()
+        # If there's an error, abort
+        except Exception as e:
+            abort(500)
+
+        return jsonify({
+            'success': True,
+            'user': added_user
         })
 
     # Endpoint: GET /messages
@@ -110,6 +161,32 @@ def create_app(test_config=None):
             'success': True,
             'messages': formatted_messages
         })
+
+    # Endpoint: POST /messages
+    # Description: Adds a new message to the messages table.
+    # Parameters: None.
+    @app.route('/messages', methods=['POST'])
+    def add_message():
+        # Gets the new message's data
+        message_data = json.loads(request.data)
+        new_message = Message(from_id=message_data['fromId'],
+                              for_id=message_data['forId'],
+                              text=message_data['text'],
+                              date=message_data['date'])
+
+        # Try to add the post to the database
+        try:
+            db_add(new_message)
+            sent_message = new_message.format()
+        # If there's an error, abort
+        except Exception as e:
+            abort(500)
+
+        return jsonify({
+            'success': True,
+            'message': sent_message
+        })
+
 
     # Error Handlers
     # -----------------------------------------------------------------
