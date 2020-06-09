3a6a609e1cbf68f5cd48889827a557e56d419108
Shir Bar Lev <shiruchii@gmail.com>
2020-06-09 11:44:01 +0300

    Added required configuration variables.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-
+export DATABASE_URL='postgresql://postgres:postgres@localhost:5432/capstone'
+export AUTH0_DOMAIN='dev-sbac.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='sendhug'
+export CLIENT_ID='rgZL4I04pep3P2GRIEVQtDkWcHjv9sru'
