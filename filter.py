cdd6e04b0a937caa70387c9748676396ee30be8c
Shir Bar Lev <shirblc@gmail.com>
2020-07-18 11:26:26 +0300

    Added method to check for blacklisted words.

--- filter.py
+++ filter.py
@@ -16,3 +16,24 @@ class Filter(Wordfilter):
     def remove_word(self, index):
         remove_index = int(index) + 66
         return self.blacklist.pop(remove_index)
+
+    # Check if there's a blacklisted word in the text
+    def blacklisted(self, string):
+        test_string = string.lower()
+        badword_indexes = []
+        is_blacklisted = False;
+
+        # Check each word
+        for badword in self.blacklist:
+            if(badword in test_string):
+                is_blacklisted = True;
+                badword_indexes.append({
+                    'badword': badword,
+                    'index': test_string.index(badword)
+                })
+
+        # return
+        return {
+            'blacklisted': is_blacklisted,
+            'indexes': badword_indexes
+        }
