1bc0f30b78b2d6db44193f7c781af4c1a8109d0f
Shir Bar Lev <shirblc@gmail.com>
2023-01-29 15:22:40 +0000

    Deleted unneeded files
    These include pyup config (which isn't used anymore), files
    for Heroku deployment (also not used anymore) and an example of
    the environment setup.

--- setup.sh
+++
@@ -1,6 +0,0 @@
-export DATABASE_URL='postgresql://postgres:postgres@localhost:5432/capstone'
-export AUTH0_DOMAIN='dev-sbac.auth0.com'
-export ALGORITHMS=['RS256']
-export API_AUDIENCE='sendhug'
-export CLIENT_ID='rgZL4I04pep3P2GRIEVQtDkWcHjv9sru'
-export FRONTEND='http://localhost:3000'
