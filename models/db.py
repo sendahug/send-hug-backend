67a6e174bde1ac9c39feaf42e727c80b5d83d31d
Shir Bar Lev <shirblc@gmail.com>
2024-04-26 17:32:44 +0300

    Deleted most of the non-async methods.

--- models/db.py
+++ models/db.py
@@ -113,8 +113,6 @@ class SendADatabase:
         app.config["SQLALCHEMY_DATABASE_URI"] = self.database_url
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
         self.app = app
-
-        self.app.teardown_appcontext(self._remove_session)
         self.app.teardown_appcontext(self._remove_async_session)
 
     @deprecated("Use _create_async_session_factory instead")
@@ -153,14 +151,6 @@ class SendADatabase:
             session_factory=self.async_session_factory, scopefunc=current_task
         )
 
-    @deprecated("Use _remove_async_session instead")
-    def _remove_session(self, exception: BaseException | None):
-        """
-        Removes the sesion once the app context is torn down.
-        Copied from Flask-SQLAlchemy.
-        """
-        self.session.remove()
-
     async def _remove_async_session(self, exception: BaseException | None):
         """ """
         await self.async_session.remove()
@@ -173,46 +163,6 @@ class SendADatabase:
 
     # READ
     # -----------------------------------------------------------------
-    @deprecated("Use async_paginate instead")
-    def paginate(
-        self,
-        query: Select,  # TODO: This select needs to be Select[tuple[CoreSAHModel]]
-        current_page: int,
-        per_page: int | None = None,
-    ) -> PaginationResult:
-        """
-        Return a paginated result from a query. This includes the items in the given
-        page, the total number of pages and the total number of items.
-
-        param query: The base query to execute.
-        param current_page: The current page to fetch the items for.
-        param per_page: The amount of items to include in each page.
-        """
-        session = self.create_scoped_session()
-
-        if per_page is None:
-            per_page = self.default_per_page
-
-        with session() as sess:
-            try:
-                items = sess.scalars(
-                    query.limit(per_page).offset((current_page - 1) * per_page)
-                ).all()
-                total_items = (
-                    sess.scalar(select(func.count()).select_from(query.cte())) or 0
-                )
-
-                return PaginationResult(
-                    resource=[item.format() for item in list(items)],
-                    current_page=current_page,
-                    per_page=per_page,
-                    total_items=total_items,
-                    total_pages=math.ceil(total_items / per_page),
-                )
-
-            except Exception as err:
-                abort(500, str(err))
-
     async def async_paginate(
         self,
         query: Select,  # TODO: This select needs to be Select[tuple[CoreSAHModel]]
@@ -255,26 +205,6 @@ class SendADatabase:
         except Exception as err:
             abort(500, str(err))
 
-    @deprecated("Use async_one_or_404 instead")
-    def one_or_404(self, item_id: int, item_type: Type[T]) -> T:
-        """
-        Fetch a single item or return 404 if it doesn't exist. Inspired by
-        Flask-SQLAlchemy 3's `get_or_404` method.
-        """
-        try:
-            item = self.session.get(item_type, item_id)
-
-            if item is None:
-                abort(404)
-
-            return item
-
-        except HTTPException as exc:
-            raise exc
-
-        except Exception as err:
-            abort(422, str(err))
-
     async def async_one_or_404(self, item_id: int, item_type: Type[T]) -> T:
         """
         Fetch a single item or return 404 if it doesn't exist. Inspired by
@@ -296,28 +226,6 @@ class SendADatabase:
 
     # CREATE
     # -----------------------------------------------------------------
-    @deprecated("Use async_add_object instead")
-    def add_object(self, obj: CoreSAHModel) -> CoreSAHModel:
-        """
-        Inserts a new record into the database.
-
-        param obj: Object to insert.
-        """
-        # Try to add the object to the database
-        try:
-            self.session.add(obj)
-            self.session.commit()
-
-            return obj
-        # If there's a database error
-        except (DataError, IntegrityError) as err:
-            self.session.rollback()
-            abort(422, str(err.orig))
-        # If there's an error, rollback
-        except Exception as err:
-            self.session.rollback()
-            abort(500, str(err))
-
     async def async_add_object(self, obj: CoreSAHModel) -> CoreSAHModel:
         """
         Inserts a new record into the database.
@@ -340,28 +248,6 @@ class SendADatabase:
             abort(500, str(err))
 
     # Bulk add
-    @deprecated("Use async_add_multiple_objects instead")
-    def add_multiple_objects(self, objects: list[CoreSAHModel]) -> list[DumpedModel]:
-        """
-        Inserts multiple records into the database.
-
-        param objects: The list of objects to add to the database.
-        """
-        # Try to add the objects to the database
-        try:
-            self.session.add_all(objects)
-            self.session.commit()
-
-            return [item.format() for item in objects]
-        # If there's a database error
-        except (DataError, IntegrityError) as err:
-            self.session.rollback()
-            abort(422, str(err.orig))
-        # If there's an error, rollback
-        except Exception as err:
-            self.session.rollback()
-            abort(500, str(err))
-
     async def async_add_multiple_objects(
         self, objects: list[CoreSAHModel]
     ) -> list[DumpedModel]:
@@ -387,28 +273,6 @@ class SendADatabase:
 
     # UPDATE
     # -----------------------------------------------------------------
-    @deprecated("Use async_update_object instead")
-    def update_object(self, obj: CoreSAHModel) -> CoreSAHModel:
-        """
-        Updates an existing record.
-
-        param obj: Updated object to commit.
-        """
-        # Try to update the object in the database
-        try:
-            self.session.commit()
-            self.session.refresh(obj)
-
-            return obj
-        # If there's a database error
-        except (DataError, IntegrityError) as err:
-            self.session.rollback()
-            abort(422, str(err.orig))
-        # If there's an error, rollback
-        except Exception as err:
-            self.session.rollback()
-            abort(500, str(err))
-
     async def async_update_object(self, obj: CoreSAHModel) -> CoreSAHModel:
         """
         Updates an existing record.
@@ -431,35 +295,6 @@ class SendADatabase:
             abort(500, str(err))
 
     # Bulk Update
-    @deprecated("Use async_update_multiple_objects instead")
-    def update_multiple_objects(
-        self, objects: Sequence[CoreSAHModel]
-    ) -> list[DumpedModel]:
-        """
-        Updates multiple records.
-
-        param objects: A list with all objects to update.
-        """
-        updated_objects = []
-
-        # Try to update the objects in the database
-        try:
-            self.session.commit()
-
-            for obj in objects:
-                self.session.refresh(obj)
-                updated_objects.append(obj.format())
-
-            return updated_objects
-        # If there's a database error
-        except (DataError, IntegrityError) as err:
-            self.session.rollback()
-            abort(422, str(err.orig))
-        # If there's an error, rollback
-        except Exception as err:
-            self.session.rollback()
-            abort(500, str(err))
-
     async def async_update_multiple_objects(
         self, objects: Sequence[CoreSAHModel]
     ) -> list[DumpedModel]:
@@ -486,38 +321,6 @@ class SendADatabase:
         # If there's an error, rollback
         except Exception as err:
             await self.async_session.rollback()
-            abort(500, str(err))
-
-    @overload
-    def update_multiple_objects_with_dml(self, update_stmts: list[Update]):
-        ...
-
-    @overload
-    def update_multiple_objects_with_dml(self, update_stmts: Update):
-        ...
-
-    @deprecated("Use async_update_multiple_objects_with_dml instead")
-    def update_multiple_objects_with_dml(self, update_stmts):
-        """
-        Updates multiple objects with a single UPDATE statement.
-
-        param update_stmt: The UPDATE statement to execute.
-        """
-        try:
-            if isinstance(update_stmts, list):
-                for stmt in cast(list[Update], update_stmts):
-                    self.session.execute(stmt)
-            else:
-                self.session.execute(update_stmts)
-
-            self.session.commit()
-        # If there's a database error
-        except (DataError, IntegrityError) as err:
-            self.session.rollback()
-            abort(422, str(err.orig))
-        # If there's an error, rollback
-        except Exception as err:
-            self.session.rollback()
             abort(500, str(err))
 
     @overload
@@ -553,27 +356,6 @@ class SendADatabase:
 
     # DELETE
     # -----------------------------------------------------------------
-    @deprecated("Use async_delete_object instead")
-    def delete_object(self, object: CoreSAHModel) -> int:
-        """
-        Deletes an existing record.
-
-        param obj: Object to delete.
-        """
-        # Try to delete the record from the database
-        try:
-            self.session.delete(object)
-            self.session.commit()
-            return object.id
-        # If there's a database error
-        except (DataError, IntegrityError) as err:
-            self.session.rollback()
-            abort(422, str(err.orig))
-        # If there's an error, rollback
-        except Exception as err:
-            self.session.rollback()
-            abort(500, str(err))
-
     async def async_delete_object(self, object: CoreSAHModel) -> int:
         """
         Deletes an existing record.
@@ -596,26 +378,6 @@ class SendADatabase:
 
     # Bulk delete
     # TODO: Return the number of deleted items
-    @deprecated("User async_delete_multiple_objects instead")
-    def delete_multiple_objects(self, delete_stmt: Delete):
-        """
-        Executes a delete statement to delete multiple objects.
-
-        param delete_stmt: The DELETE statement to execute.
-        """
-        # Try to delete the objects from the database
-        try:
-            self.session.execute(delete_stmt)
-            self.session.commit()
-        # If there's a database error
-        except (DataError, IntegrityError) as err:
-            self.session.rollback()
-            abort(422, str(err.orig))
-        # If there's an error, rollback
-        except Exception as err:
-            self.session.rollback()
-            abort(500, str(err))
-
     async def async_delete_multiple_objects(self, delete_stmt: Delete):
         """
         Executes a delete statement to delete multiple objects.
