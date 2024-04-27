4a8aa0e092f6d2a35944c25db003434ccbdb5ab1
Shir Bar Lev <shirblc@gmail.com>
2024-04-27 15:39:17 +0300

    Added some logging to see what the problem is.

--- tests/test_users.py
+++ tests/test_users.py
@@ -766,6 +766,7 @@ async def test_user_hugs(app_client, test_db, user_headers):
         "/users/all/1/hugs", headers=user_headers["moderator"]
     )
     response_data = await response.get_json()
+    print(response_data)
 
     assert response_data["success"] is True
     assert response.status_code == 200
